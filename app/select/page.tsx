a89727ff80b7c7bd64fc31a5921ff724d38b784a
justinron31 <justingalangzxc31@gmail.com>
2025-02-16 15:13:49 +0800

    fix layout column

--- app/select/page.tsx
+++ app/select/page.tsx
@@ -18,7 +18,7 @@ export default function Select() {
             </h1>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 gap-3">
             {/* Layout 1 */}
             <button
               onClick={() => setSelectedLayout(2)}
