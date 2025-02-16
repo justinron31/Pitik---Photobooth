7d8d04e461985c2b9a9a0f89624a23c6b0c3438a
justinron31 <justingalangzxc31@gmail.com>
2025-02-16 15:29:12 +0800

    updated

---
+++ app/api/sitemap/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const pages = ["/", "/select", "/capture", "/share"];
+  const domain = "https://pitikbooth.vercel.app/";
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map(
+        (page) => `
+      <url>
+        <loc>${domain}${page}</loc>
+        <priority>${page === "/" ? "1.0" : "0.8"}</priority>
+      </url>`
+      )
+      .join("")}
+  </urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: { "Content-Type": "application/xml" },
+  });
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,50 @@ import type { Metadata } from "next";
 import Footer from "@/components/footer";
 import "./globals.css";
 import "animate.css";
+
 export const metadata: Metadata = {
-  title: "Pitik – Photobooth",
+  title: "Pitik – Virtual Photobooth",
   description:
     "Experience a hassle-free virtual photobooth. No sign-up required! Capture stunning photos, choose stylish frames, and download instantly.",
+  keywords: [
+    "pitik photobooth",
+    "virtual photobooth",
+    "online photobooth",
+    "photo capture",
+    "selfie booth",
+    "instant download photos",
+  ],
+  authors: [{ name: "K1dla", url: "https://justinron.vercel.app/" }],
+  metadataBase: new URL("https://pitikbooth.vercel.app/"),
+  openGraph: {
+    title: "Pitik – Virtual Photobooth",
+    description:
+      "Capture stunning photos with our hassle-free virtual photobooth. No sign-up required, just snap, style, and download!",
+    url: "https://pitikbooth.vercel.app/",
+    siteName: "Pitik Photobooth",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Pitik Photobooth Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pitik – Virtual Photobooth",
+    description:
+      "Capture stunning photos with our hassle-free virtual photobooth. No sign-up required, just snap, style, and download!",
+    images: ["/og-image.png"],
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
